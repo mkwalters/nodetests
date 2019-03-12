9a835568373a8d1e8a0fe81974aa6a05ed1f487d
Mitchell Walters <mitchellwalters@Mitchells-MacBook-Pro.local>
2019-03-12 15:23:24 -0700

    primordial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let request = require('request');
+
+let apiKey = 'b54f61e034024fb9d77f7c46f05b78c2';
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
