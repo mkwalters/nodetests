4fe4826ae76a471c7c6e5b58c9d790b6574d7bb3
Mitchell Walters <mitchellwalters@Mitchells-MacBook-Pro.local>
2019-03-12 21:56:21 -0700

    cool

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 let request = require('request');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let apiKey = 'b54f61e034024fb9d77f7c46f05b78c2';
 let city = 'portland';
@@ -15,6 +19,28 @@ request(url, function (err, response, body) {
   }
 });
 
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+})
 
 app.get('/', function (req, res) {
   res.render('index');
